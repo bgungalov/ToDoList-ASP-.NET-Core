15ba2676ac28cbfc5d5c9e6124748a8354c12957
Biser <Biser@DESKTOP-6CE6O6J>
2021-09-25 18:21:29 +0300

    Add project files.

---
+++ ToDoList/Migrations/20210925133639_InitialCreate.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ToDoList.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "ToDoItem",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Title = table.Column<string>(type: "nvarchar(200)", maxLength: 200, nullable: false),
+                    Content = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Date = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    Time = table.Column<DateTime>(type: "datetime2", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ToDoItem", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ToDoItem");
+        }
+    }
+}
---
+++ ToDoList/Models/ToDoItem.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ToDoList.Models
+{
+    public class ToDoItem
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [StringLength(200)]
+        [Display(Name = "Title")]
+        [Required(ErrorMessage = "Title is required and can't be longer than 200 characters")]
+        public string Title { get; set; }
+
+        [Display(Name = "Content")]
+        public string Content { get; set; }
+
+        [DataType(DataType.Date)]
+        [Display(Name = "Date")]
+        public DateTime? Date { get; set; }
+
+        [DataType(DataType.Time)]
+        [Display(Name = "Time")]
+        public DateTime? Time { get; set; }
+    }
+}
