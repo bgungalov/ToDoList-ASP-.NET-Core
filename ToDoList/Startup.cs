eaa133fe02243391ea674ac12596d0246384a3ff
Biser Gungalov <biskata502@gmail.com>
2021-09-25 19:27:49 +0300

    fixed routing and buttons

--- ToDoList/Startup.cs
+++ ToDoList/Startup.cs
@@ -55,7 +55,7 @@ namespace ToDoList
             {
                 endpoints.MapControllerRoute(
                     name: "default",
-                    pattern: "{controller=ToDoItems}/{action=Create}/{id?}");
+                    pattern: "{controller=ToDoItems}/{action=Index}/{id?}");
             });
         }
     }
